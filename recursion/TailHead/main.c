2bbd1632aefbf3b34b1f28eab5c77aa468505c67
yeumkyuseok <yeumkyuseok@gmail.com>
2021-07-31 15:04:12 +0900

    initialized recursion directory

---
+++ recursion/TailHead/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void fun(int n)
+{
+    if (n > 0) {
+        printf("%d ", n);
+        fun(n - 1);
+    }
+}
+
+int main(void) 
+{
+    int x = 3;
+    fun(x);
+
+    return 0;
+}
