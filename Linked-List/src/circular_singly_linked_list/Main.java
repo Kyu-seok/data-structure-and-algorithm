a765b7944492c4e69f56bb0035520a7f8ab2358a
Kyu <yeumkyuseok@gmail.com>
2022-02-28 10:42:00 +0900

    Initialize classes for circular singly linked list

--- Linked-List/src/circular_singly_linked_list/Main.java
+++ Linked-List/src/circular_singly_linked_list/Main.java
@@ -5,8 +5,12 @@ public class Main {
     public static void main(String[] args) {
         CircularSinglyLinkedList csll = new CircularSinglyLinkedList();
         csll.createCSLL(5);
-        System.out.println(csll.head.value);
-        System.out.println(csll.head.next.value);
+        csll.insertCSLL(4, 0);
+        csll.insertCSLL(6, 1);
+        csll.insertCSLL(7, 8);
+        csll.traverseCSLL();
+        csll.deleteCSLL();
+        csll.traverseCSLL();
     }
 
 }
