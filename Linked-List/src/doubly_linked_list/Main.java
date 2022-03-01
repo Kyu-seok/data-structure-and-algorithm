006ecadd2787eb2a1e28336134ffdb8c468c60cc
Kyu <yeumkyuseok@gmail.com>
2022-03-01 11:51:00 +0900

    Implement doubly-linked list

--- Linked-List/src/doubly_linked_list/Main.java
+++ Linked-List/src/doubly_linked_list/Main.java
@@ -1,4 +1,16 @@
 package doubly_linked_list;
 
 public class Main {
+
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.createDLL(1);
+        dll.insertDLL(2, 0);
+        dll.insertDLL(3, 1);
+        dll.insertDLL(4, 7);
+        dll.traverseDLL();
+        dll.deleteDLL();
+        dll.traverseDLL();
+    }
+
 }
