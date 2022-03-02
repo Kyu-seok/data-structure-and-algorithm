316b53c0505873d5657ef623f4a253c20e7a4ed9
Kyu <yeumkyuseok@gmail.com>
2022-03-02 11:00:58 +0900

    Implement insertion method

--- Linked-List/src/circular_doubly_linked_list/CircularDoublyLinkedList.java
+++ Linked-List/src/circular_doubly_linked_list/CircularDoublyLinkedList.java
@@ -19,4 +19,38 @@ public class CircularDoublyLinkedList {
         return head;
     }
 
+    // Insertion Method
+    void insertNode(int nodeValue, int location) {
+        DoublyNode newNode = new DoublyNode();
+        newNode.value = nodeValue;
+        if (head == null) {
+            createCDLL(nodeValue);
+            return;
+        } else if (location == 0) {
+            newNode.next = head;
+            newNode.prev = tail;
+            head.prev = newNode;
+            tail.next = newNode;
+            head = newNode;
+        } else if (location >= size) {
+            newNode.next = head;
+            newNode.prev = tail;
+            head.prev = newNode;
+            tail.next = newNode;
+            tail = newNode;
+        } else {
+            DoublyNode tempNode = head;
+            int index = 0;
+            while(index < location - 1) {
+                tempNode = tempNode.next;
+                index++;
+            }
+            newNode.prev = tempNode;
+            newNode.next = tempNode.next;
+            tempNode.next = newNode;
+            newNode.next.prev = newNode;
+        }
+        size++;
+    }
+
 }
--- Linked-List/src/circular_doubly_linked_list/Main.java
+++ Linked-List/src/circular_doubly_linked_list/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         CircularDoublyLinkedList cdll = new CircularDoublyLinkedList();
         cdll.createCDLL(1);
+        cdll.insertNode(2, 0);
         System.out.println(cdll.head.value);
     }
 }
