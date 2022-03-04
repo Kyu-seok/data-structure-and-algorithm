1e5a6915ce47be2d60586edcda4ce4969982a1c5
Kyu <yeumkyuseok@gmail.com>
2022-03-04 12:17:18 +0900

    Implement deleteNode() method

--- Linked-List/src/circular_doubly_linked_list/CircularDoublyLinkedList.java
+++ Linked-List/src/circular_doubly_linked_list/CircularDoublyLinkedList.java
@@ -103,4 +103,46 @@ public class CircularDoublyLinkedList {
         return false;
     }
 
+    // Deletion Method
+    public void deleteNode(int location) {
+        if (head == null) {
+            System.out.println("The CDLL does not exists");
+            return;
+        } else if (location == 0) {
+            if (size == 1) {
+                head.prev = null;
+                head.next = null;
+                head = tail = null;
+                size--;
+                return;
+            } else {
+                head = head.next;
+                head.prev = tail;
+                tail.next = head;
+                size--;
+            }
+        } else if (location >= size) {
+            if (size == 1) {
+                head.prev = null;
+                head.next = null;
+                head = tail = null;
+                size--;
+                return;
+            } else {
+                tail = tail.prev;
+                tail.next = head;
+                head.prev = tail;
+                size--;
+            }
+        } else {
+            DoublyNode tempNode = head;
+            for (int i = 0; i < location - 1; i++) {
+                tempNode = tempNode.next;
+            }
+            tempNode.next = tempNode.next.next;
+            tempNode.next.prev = tempNode;
+            size--;
+        }
+    }
+
 }
