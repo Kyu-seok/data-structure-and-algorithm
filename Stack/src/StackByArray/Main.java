7de449b7bfb2b544066f8e8aa3f1467b13160202
Kyu <yeumkyuseok@gmail.com>
2022-03-15 10:09:03 +0900

    Implement Stack using Array

--- Stack/src/StackByArray/Main.java
+++ Stack/src/StackByArray/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         Stack newStack = new Stack(4);
+        newStack.push(1);
+        newStack.push(2);
+        newStack.push(3);
+        newStack.push(4);
+        newStack.deleteStack();
     }
 
 }
--- Stack/src/StackByArray/Stack.java
+++ Stack/src/StackByArray/Stack.java
@@ -10,4 +10,47 @@ public class Stack {
         System.out.println("The Stack is created with size of: " + size);
     }
 
+    public boolean isEmpty() {
+        return topOfStack == -1 ? true : false;
+    }
+
+    public boolean isFull() {
+        return topOfStack == arr.length - 1 ? true : false;
+    }
+
+    public void push(int value) {
+        if (isFull()) {
+            System.out.println("The stack is full!");
+        } else {
+            arr[topOfStack + 1] = value;
+            topOfStack++;
+            System.out.println("The value is successfully inserted");
+        }
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("The stack is empty");
+            return -1;
+        } else {
+            int topStack = arr[topOfStack];
+            topOfStack--;
+            return topStack;
+        }
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("The stack is empty");
+            return -1;
+        } else {
+            return arr[topOfStack];
+        }
+    }
+
+    public void deleteStack() {
+        arr = null;
+        System.out.println("The Stack is successfully deleted");
+    }
+
 }
