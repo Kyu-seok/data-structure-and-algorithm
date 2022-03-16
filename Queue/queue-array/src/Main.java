663f16884dd52d8a15c80c024d55133e42f6072a
Kyu <yeumkyuseok@gmail.com>
2022-03-16 13:00:46 +0900

    Implement isFull(), isEmpty(), enQueue() in Queue Array

--- Queue/queue-array/src/Main.java
+++ Queue/queue-array/src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+
+    public static void main(String[] args) {
+        QueueArray newQueue = new QueueArray(3);
+        newQueue.enQueue(1);
+        newQueue.enQueue(2);
+        newQueue.enQueue(3);
+        newQueue.enQueue(4);
+    }
+
 }
--- Queue/queue-array/src/QueueArray.java
+++ Queue/queue-array/src/QueueArray.java
@@ -1,2 +1,45 @@
 public class QueueArray {
+
+    int[] arr;
+    int topOfQueue;
+    int beginningOfQueue;
+
+    public QueueArray(int size) {
+        this.arr = new int[size];
+        this.topOfQueue = -1;
+        this.beginningOfQueue = -1;
+        System.out.println("The Queue is successfully created with size of: " + size);
+    }
+
+    public boolean isFull() {
+        if (topOfQueue == arr.length - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isEmpty() {
+        if (beginningOfQueue == -1 || beginningOfQueue == arr.length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void enQueue(int value) {
+        if (isFull()) {
+            System.out.println("The Queue is Full!");
+        } else if (isEmpty()) {
+            beginningOfQueue = 0;
+            topOfQueue++;
+            arr[topOfQueue] = value;
+            System.out.println("Successfully inserted " + value + " in the queue");
+        } else {
+            topOfQueue++;
+            arr[topOfQueue] = value;
+            System.out.println("Successfully inserted " + value + " in the queue");
+        }
+    }
+
 }
