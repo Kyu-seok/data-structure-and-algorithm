a65f90a595a5f47e64109cbbc6d665326648bcc8
Kyu <yeumkyuseok@gmail.com>
2022-03-19 18:11:43 +0900

    Implement Dequeue(), Peek(), Delete() in Circular Queue using Array

--- Queue/circular-queue/src/CircularQueue.java
+++ Queue/circular-queue/src/CircularQueue.java
@@ -1,2 +1,85 @@
 public class CircularQueue {
+
+    int[] arr;
+    int topOfQueue;
+    int beginningOfQueue;
+    int size;
+
+    public CircularQueue(int size) {
+        this.arr = new int[size];
+        this.size = size;
+        this.topOfQueue = -1;
+        this.beginningOfQueue = -1;
+        System.out.println("The CQ is successfully created with size of " + size);
+    }
+
+    public boolean isEmpty() {
+        if (topOfQueue == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isFull() {
+        if (topOfQueue + 1 == beginningOfQueue) {
+            return true;
+        } else if (beginningOfQueue == 0 && topOfQueue + 1 == size) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void enQueue(int value) {
+        if (isFull()) {
+            System.out.println("The CQ is full!");
+        } else if (isEmpty()) {
+            beginningOfQueue = 0;
+            topOfQueue++;
+            arr[topOfQueue] = value;
+            System.out.println("Successfully inserted " + value + " in the queue");
+        } else {
+            if (topOfQueue + 1 == size) {
+                topOfQueue = 0;
+            } else {
+                topOfQueue++;
+            }
+            arr[topOfQueue] = value;
+            System.out.println("Successfully inserted " + value + " in the queue");
+        }
+    }
+
+    public int deQueue() {
+        if (isEmpty()) {
+            System.out.println("The CQ is empty!");
+            return -1;
+        } else {
+            int result = arr[beginningOfQueue];
+            arr[beginningOfQueue] = 0;
+            if (beginningOfQueue == topOfQueue) {
+                beginningOfQueue = topOfQueue = -1;
+            } else if (beginningOfQueue + 1 == size) {
+                beginningOfQueue = 0;
+            } else {
+                beginningOfQueue++;
+            }
+            return result;
+        }
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("The CQ is empty");
+            return -1;
+        } else {
+            return arr[beginningOfQueue];
+        }
+    }
+
+    public void deleteQueue() {
+        arr = null;
+        System.out.println("The CQ is successfully deleted!");
+    }
+
 }
--- Queue/circular-queue/src/Main.java
+++ Queue/circular-queue/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        CircularQueue newCQ = new CircularQueue(3);
+        newCQ.enQueue(10);
+        newCQ.enQueue(20);
+        newCQ.enQueue(30);
+        newCQ.deleteQueue();
     }
+
 }
