1c4e317bb3ec86a5b7b7d27b3b36e4d4fbe18c9b
Kyu <yeumkyuseok@gmail.com>
2022-03-20 18:40:43 +0900

    Implement create(), enQueue(), isEmpty() in the Queue using Linked List

--- Queue/queue-linked-list/src/Main.java
+++ Queue/queue-linked-list/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        QueueLinkedList newQLL = new QueueLinkedList();
+        newQLL.enQueue(10);
+        newQLL.enQueue(20);
+        newQLL.enQueue(30);
+        boolean result = newQLL.isEmpty();
+        System.out.println(result);
     }
 }
--- Queue/queue-linked-list/src/QueueLinkedList.java
+++ Queue/queue-linked-list/src/QueueLinkedList.java
@@ -1,2 +1,21 @@
 public class QueueLinkedList {
+    LinkedList list;
+
+    public QueueLinkedList() {
+        list = new LinkedList();
+        System.out.println("The Queue is successfully created");
+    }
+
+    public boolean isEmpty() {
+        if (list.head == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void enQueue(int value) {
+        list.insertInLinkedList(value, list.size);
+        System.out.println("Successfully inserted " + value + " in the queue");
+    }
 }
