db73ffd157e57fd508e580a60b866be5a3995d87
Kyu-seok <yeumkyuseok@gmail.com>
2022-03-26 14:50:53 +0900

    Implement preOrder(), inOrder(), postOrder(), levelOrder() traverse methods

--- Tree/src/binaryTree/BinaryTreeLL.java
+++ Tree/src/binaryTree/BinaryTreeLL.java
@@ -1,5 +1,8 @@
 package binaryTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTreeLL {
     BinaryNode root;
 
@@ -7,5 +10,50 @@ public class BinaryTreeLL {
         this.root = null;
     }
 
+    // PreOrder Traversal
+    void preOrder(BinaryNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.value + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    // InOrder Traversal
+    void inOrder(BinaryNode node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.value + " ");
+        inOrder(node.right);
+    }
+
+    // PostOrder Traversal
+    void postOrder(BinaryNode node) {
+        if (node == null) {
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.value + " ");
+    }
+
+    // LevelOrder Traversal
+    void levelOrder() {
+        Queue<BinaryNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            BinaryNode presentNode = queue.remove();
+            System.out.print(presentNode.value + " ");
+            if (presentNode.left != null) {
+                queue.add(presentNode.left);
+            }
+            if (presentNode.right != null) {
+                queue.add(presentNode.right);
+            }
+        }
+    }
 
 }
