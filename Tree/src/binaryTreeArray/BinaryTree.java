a60782730c793f65ab95c85677d7ad47b0351d45
Kyu-seok <yeumkyuseok@gmail.com>
2022-03-31 21:59:16 +0900

    Implement insert()

--- Tree/src/binaryTreeArray/BinaryTree.java
+++ Tree/src/binaryTreeArray/BinaryTree.java
@@ -9,4 +9,24 @@ public class BinaryTree {
         this.lastUsedIndex = 0;
         System.out.println("Blank Tree of size " + size + " has been created");
     }
+
+    boolean isFull() {
+        if (arr.length - 1 == lastUsedIndex) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // insert method
+    void insert(String value) {
+        if (!isFull()) {
+            arr[lastUsedIndex + 1] = value;
+            lastUsedIndex++;
+            System.out.println("The value of " + value + " has been inserted");
+        } else {
+            System.out.println("The BT is full");
+        }
+    }
+
 }
