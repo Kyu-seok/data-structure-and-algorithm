3a867896ffec902ab6c133af19e3926c97b8c09b
Kyu-seok <yeumkyuseok@gmail.com>
2022-04-01 22:10:40 +0900

    Final

--- Tree/src/binaryTree/Main.java
+++ Tree/src/binaryTree/Main.java
@@ -3,6 +3,12 @@ package binaryTree;
 public class Main {
     public static void main(String[] args) {
         BinaryTreeLL binaryTree = new BinaryTreeLL();
-
+        binaryTree.insert("N1");
+        binaryTree.insert("N2");
+        binaryTree.insert("N3");
+        binaryTree.insert("N4");
+        binaryTree.insert("N5");
+        binaryTree.insert("N6");
+        binaryTree.deleteBT();
     }
 }
--- Tree/src/binaryTreeArray/Main.java
+++ Tree/src/binaryTreeArray/Main.java
@@ -1,7 +1,20 @@
 package binaryTreeArray;
 
 public class Main {
+
     public static void main(String[] args) {
-        BinaryTree newBinaryTree = new BinaryTree(5);
+        BinaryTree newBinaryTree = new BinaryTree(9);
+        newBinaryTree.insert("N1");
+        newBinaryTree.insert("N2");
+        newBinaryTree.insert("N3");
+        newBinaryTree.insert("N4");
+        newBinaryTree.insert("N5");
+        newBinaryTree.insert("N6");
+        newBinaryTree.insert("N7");
+        newBinaryTree.insert("N8");
+        newBinaryTree.insert("N9");
+        System.out.println();
+        newBinaryTree.preOrder(1);
     }
+
 }
