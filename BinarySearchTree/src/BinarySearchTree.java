20f4a695649627efdc536d86a8fc9767294705f0
Kyu-seok <yeumkyuseok@gmail.com>
2022-04-04 11:55:53 +0900

    Implement insert(), preOrder(), inOrder(), postOrder(), levelOrder() methods

--- BinarySearchTree/src/BinarySearchTree.java
+++ BinarySearchTree/src/BinarySearchTree.java
@@ -1,7 +1,73 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class BinarySearchTree {
     BinaryNode root;
 
     public BinarySearchTree() {
-        this.root = null;
+        root = null;
     }
+
+    private BinaryNode insert(BinaryNode currentNode, int value) {
+        if (currentNode == null) {
+            BinaryNode newNode = new BinaryNode();
+            newNode.value = value;
+            // System.out.println("The value successfully inserted");
+            return newNode;
+        } else if (value <= currentNode.value) {
+            currentNode.left = insert(currentNode.left, value);
+            return currentNode;
+        } else {
+            currentNode.right = insert(currentNode.right, value);
+            return currentNode;
+        }
+    }
+
+    void insert(int value) {
+        root = insert(root, value);
+    }
+
+    public void preOrder(BinaryNode node) {
+        if (node == null) {
+           return;
+        }
+        System.out.print(node.value + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder(BinaryNode node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.value + " ");
+        inOrder(node.right);
+    }
+
+    public void postOrder(BinaryNode node) {
+        if (node == null) {
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.value + " ");
+    }
+
+    public void levelOrder() {
+        Queue<BinaryNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            BinaryNode presentNode = queue.remove();
+            System.out.print(presentNode.value + " ");
+            if (presentNode.left != null) {
+                queue.add(presentNode.left);
+            }
+            if (presentNode.right != null) {
+                queue.add(presentNode.right);
+            }
+        }
+    }
+
+
 }
