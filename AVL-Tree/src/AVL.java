bf631fd07b0e438183f354f60d43e3e1e26c7a04
Kyu-seok <yeumkyuseok@gmail.com>
2022-04-06 15:14:28 +0900

    Implement insert() method using leftRotate() and rightRotate()

--- AVL-Tree/src/AVL.java
+++ AVL-Tree/src/AVL.java
@@ -64,4 +64,77 @@ public class AVL {
         }
     }
 
+    public int getHeight(BinaryNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.height;
+    }
+
+    private BinaryNode rotateRight(BinaryNode disbalancedNode) {
+        BinaryNode newRoot = disbalancedNode.left;
+        disbalancedNode.left = disbalancedNode.left.right;
+        newRoot.right = disbalancedNode;
+        disbalancedNode.height = 1 + Math.max(getHeight(disbalancedNode.left), getHeight(disbalancedNode.right));
+        newRoot.height = 1 + Math.max(getHeight(newRoot.left), getHeight(newRoot.right));
+        return newRoot;
+    }
+
+    private BinaryNode rotateLeft(BinaryNode disbalancedNode) {
+        BinaryNode newRoot = disbalancedNode.right;
+        disbalancedNode.right = disbalancedNode.right.left;
+        newRoot.left = disbalancedNode;
+        disbalancedNode.height = 1 + Math.max(getHeight(disbalancedNode.left), getHeight(disbalancedNode.right));
+        newRoot.height = 1 + Math.max(getHeight(newRoot.left), getHeight(newRoot.right));
+        return newRoot;
+    }
+
+    public int getBalance(BinaryNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
+    private BinaryNode insertNode(BinaryNode node, int nodeValue) {
+        if (node == null) {
+            BinaryNode newNode = new BinaryNode();
+            newNode.value = nodeValue;
+            newNode.height = 1;
+            return newNode;
+        } else if (nodeValue < node.value) {
+            node.left = insertNode(node.left, nodeValue);
+        } else {
+            node.right = insertNode(node.right, nodeValue);
+        }
+
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+        int balance = getBalance(node);
+
+        if (balance > 1 && nodeValue < node.left.value) {
+            return rotateRight(node);
+        }
+
+        if (balance > 1 && nodeValue > node.left.value) {
+            node.left = rotateLeft(node.left);
+            return rotateRight(node);
+        }
+
+        if (balance < -1 && nodeValue > node.right.value) {
+            return rotateLeft(node);
+        }
+
+        if (balance < -1 && nodeValue < node.right.value) {
+            node.right = rotateRight(node.right);
+            return rotateLeft(node);
+        }
+
+        return node;
+    }
+
+    public void insert(int value) {
+        root = insertNode(root, value);
+    }
+
+
 }
