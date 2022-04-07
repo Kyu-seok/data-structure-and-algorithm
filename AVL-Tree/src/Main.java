aa2b736b45f76ed34706f0a74f97b5ae5d397c15
Kyu-seok <yeumkyuseok@gmail.com>
2022-04-07 15:51:05 +0900

    Implement deleteAVL()

--- AVL-Tree/src/Main.java
+++ AVL-Tree/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         AVL newAVL = new AVL();
+        newAVL.insert(5);
+        newAVL.insert(10);
+        newAVL.insert(15);
+        newAVL.insert(20);
+        newAVL.levelOrder();
+        newAVL.delete(5);
+        System.out.println();
+        newAVL.levelOrder();
+
     }
 }
