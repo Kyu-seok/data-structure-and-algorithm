a08bb8f77af904225b4cb49549b50cb7edf3b57f
Kyu-seok <yeumkyuseok@gmail.com>
2022-04-14 18:03:25 +0900

    Implement insert & heapify function

--- BinaryHeap/src/BinaryHeap.java
+++ BinaryHeap/src/BinaryHeap.java
@@ -25,10 +25,38 @@ public class BinaryHeap {
     }
 
     public void levelOrder() {
-        for (int i = 1; i < sizeOfTree; i++) {
+        for (int i = 1; i <= sizeOfTree; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println("\n");
+    }
+
+    public void heapifyBottomToTop(int index, String heapType) {
+        int parent = index / 2;
+        if (index <= 1) {
+            return;
+        }
+        if (heapType == "Min") {
+            if (arr[index] < arr[parent]) {
+                int tmp = arr[index];
+                arr[index] = arr[parent];
+                arr[parent] = tmp;
+            }
+        } else if (heapType == "Max") {
+            if (arr[index] > arr[parent]) {
+                int tmp = arr[index];
+                arr[index] = arr[parent];
+                arr[parent] = tmp;
+            }
+        }
+        heapifyBottomToTop(parent, heapType);
+    }
+
+    public void insert(int value, String heapType) {
+        arr[sizeOfTree + 1] = value;
+        sizeOfTree++;
+        heapifyBottomToTop(sizeOfTree, heapType);
+        System.out.println("Inserted " + value + " Successfully in Heap");
     }
 
 }
