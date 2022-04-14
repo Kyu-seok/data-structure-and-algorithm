a32f08feb776e73f8bd0a03bc65f6de4cc61f0ad
Kyu-seok <yeumkyuseok@gmail.com>
2022-04-14 17:28:01 +0900

    Initialize class "BinaryHeap.java"

---
+++ BinaryHeap/src/BinaryHeap.java
@@ -0,0 +1,10 @@
+public class BinaryHeap {
+    int arr[];
+    int sizeOfTree;
+
+    public BinaryHeap(int size) {
+        arr = new int[size + 1];
+        this.sizeOfTree = 0;
+        System.out.println("Binary Heap has been created");
+    }
+}
