87f2a4ab5dc35917328a5656ae7f6e4dfa01bdb3
Kyu-seok <yeumkyuseok@gmail.com>
2022-04-16 18:47:11 +0900

    Final

--- BinaryHeap/src/Main.java
+++ BinaryHeap/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
+        BinaryHeap newBH = new BinaryHeap(5);
+        newBH.insert(10, "Max");
+        newBH.insert(5, "Max");
+        newBH.insert(15, "Max");
+        newBH.insert(1, "Max");
+        newBH.levelOrder();
+        newBH.extractHeadOfBP("Max");
+        newBH.levelOrder();
+        newBH.deleteBH();
     }
+
 }
