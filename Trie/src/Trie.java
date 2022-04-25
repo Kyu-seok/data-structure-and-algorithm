da4d53aa02813eb8711d83d86886eb5555368658
Kyu-seok <yeumkyuseok@gmail.com>
2022-04-25 13:42:06 +0900

    Implement insert()

--- Trie/src/Trie.java
+++ Trie/src/Trie.java
@@ -7,4 +7,19 @@ public class Trie {
         System.out.println("The Trie has been created");
     }
 
+    public void insert(String word) {
+        TrieNode current = root;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            TrieNode node = current.children.get(ch);
+            if (node == null) {
+                node = new TrieNode();
+                current.children.put(ch, node);
+            }
+            current = node;
+        }
+        current.endOfString = true;
+        System.out.println("Successfully inserted " + word + " in Trie");
+    }
+
 }
