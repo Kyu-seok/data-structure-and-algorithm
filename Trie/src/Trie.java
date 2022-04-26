bf2f42dbe1226e5352b57406cfed6c8272ebf96c
Kyu-seok <yeumkyuseok@gmail.com>
2022-04-26 13:59:59 +0900

    Implement search()

--- Trie/src/Trie.java
+++ Trie/src/Trie.java
@@ -22,4 +22,24 @@ public class Trie {
         System.out.println("Successfully inserted " + word + " in Trie");
     }
 
+    public boolean search(String word) {
+        TrieNode currentNode = root;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            TrieNode node = currentNode.children.get(ch);
+            if (node == null) {
+                System.out.println("Word: " + word + " does not exist in Trie");
+                return false;
+            }
+            currentNode = node;
+        }
+        if (currentNode.endOfString == true) {
+            System.out.println("Word: " + word + " exists in Trie");
+            return true;
+        } else {
+            System.out.println("Word: " + word + " does not exist in Trie. But it is a prefix of another string");
+        }
+        return currentNode.endOfString;
+    }
+
 }
