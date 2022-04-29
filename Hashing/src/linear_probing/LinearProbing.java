0b307a7a13d41546ff76eb841a41f9900fd4619b
Kyu-seok <yeumkyuseok@gmail.com>
2022-04-29 18:34:35 +0900

    Initialize linear_probing package

---
+++ Hashing/src/linear_probing/LinearProbing.java
@@ -0,0 +1,16 @@
+package linear_probing;
+
+import java.util.ArrayList;
+
+public class LinearProbing {
+    String[] hashTable;
+    int usedCellNumber;
+
+    LinearProbing(int size) {
+        hashTable = new String[size];
+        usedCellNumber = 0;
+    }
+
+
+
+}
---
+++ Hashing/src/linear_probing/Main.java
@@ -0,0 +1,4 @@
+package linear_probing;
+
+public class Main {
+}
