41a8cdb40618a09c2043778a4dc86968c340090b
Kyu-seok <yeumkyuseok@gmail.com>
2022-04-29 18:35:26 +0900

    Implement modASCIIHashFunction()

--- Hashing/src/linear_probing/LinearProbing.java
+++ Hashing/src/linear_probing/LinearProbing.java
@@ -11,6 +11,16 @@ public class LinearProbing {
         usedCellNumber = 0;
     }
 
+    public int modASCIIHashFunction(String word, int M) {
+        char[] ch;
+        ch = word.toCharArray();
+        int i, sum;
+        for (sum = 0, i = 0; i < word.length(); i++) {
+            sum = sum + ch[i];
+        }
+        return sum % M;
+    }
+
 
 
 }
