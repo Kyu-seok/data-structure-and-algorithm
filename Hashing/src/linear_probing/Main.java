dac5228a3960be5929a289d9de6f71aec70f90b8
Kyu-seok <yeumkyuseok@gmail.com>
2022-05-04 18:58:54 +0900

    Final linear_probing

--- Hashing/src/linear_probing/Main.java
+++ Hashing/src/linear_probing/Main.java
@@ -1,4 +1,15 @@
 package linear_probing;
 
 public class Main {
+
+    public static void main(String[] args) {
+        LinearProbing linearProbing = new LinearProbing(13);
+        linearProbing.insertInHashTable("The");
+        linearProbing.insertInHashTable("quick");
+        linearProbing.insertInHashTable("brown");
+        linearProbing.insertInHashTable("fox");
+        linearProbing.insertInHashTable("over");
+        linearProbing.deleteKeyHashTable("over");
+        linearProbing.displayHashTable();
+    }
 }
