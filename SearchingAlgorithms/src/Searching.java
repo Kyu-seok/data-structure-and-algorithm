02a19ad77225c6eab6d964d7e710784f966eb669
Kyu-seok <yeumkyuseok@gmail.com>
2022-05-06 19:32:41 +0900

    Implement linearSearch() in Searching.java

--- SearchingAlgorithms/src/Searching.java
+++ SearchingAlgorithms/src/Searching.java
@@ -1,2 +1,14 @@
 public class Searching {
+
+    public static int linearSearch(int arr[], int value) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                System.out.println("The element is found at the index: " + i);
+                return i;
+            }
+        }
+        System.out.println("The element " + value + " not found.");
+        return -1;
+    }
+
 }
