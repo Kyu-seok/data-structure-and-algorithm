1d691d95d98d7e19b55be741c4b50f6f17241235
Kyu-seok <yeumkyuseok@gmail.com>
2022-05-07 20:29:42 +0900

    Implement comparison methods

--- SearchingAlgorithms/src/Searching.java
+++ SearchingAlgorithms/src/Searching.java
@@ -1,23 +1,26 @@
 public class Searching {
 
-    public static int linearSearch(int arr[], int value) {
+    private int linearSearch(int arr[], int value) {
         // may throw exception if input is negative
+        int oN = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == value) {
-                System.out.println("The element is found at the index: " + i);
-                return i;
+                System.out.println("[LINEAR_SEARCH] : The element is found with O[" + oN + "]");
+                // return i;
+                return oN;
             }
+            oN++;
         }
-        System.out.println("The element " + value + " not found.");
+        System.out.println("[LINEAR_SEARCH] : The element " + value + " not found.");
         return -1;
     }
 
-    public static int binarySearch(int arr[], int value) {
+    private int binarySearch(int arr[], int value) {
         // may throw exception if input is negative
+        int oN = 0;
         int start = 0;
         int end = arr.length - 1;
         int middle = (start + end) / 2;
-        System.out.println(start + " " + middle + " " + end );
         while (arr[middle] != value && start <= end) {
             if (value < arr[middle]) {
                 end = middle - 1;
@@ -25,15 +28,45 @@ public class Searching {
                 start = middle + 1;
             }
             middle = (start + end) / 2;
-            System.out.println(start + " " + middle + " " + end );
+            oN++;
         }
         if (arr[middle] == value) {     // return (arr[middle] == value) ? middle : -1;
-            System.out.println("The element is found at the index: " + middle);
-            return middle;
+            System.out.println("[BINARY_SEARCH] : The element is found with O[" + oN + "]");
+            return oN;
         } else {
-            System.out.println("The element " + value + " not found.");
+            System.out.println("[BINARY_SEARCH] : The element " + value + " not found.");
             return -1;
         }
+    }
+
+    public void case1024(int value) {
+        int arr[] = generateArray(1024);
+        int linearON, binaryON;
+        System.out.println("\n------------------------CASE 1024------------------------\n");
+        linearON = linearSearch(arr, value);
+        binaryON = binarySearch(arr, value);
+        // if any of them is -1, print error
+        double ratio =  (double) linearON / (double) binaryON;
+        System.out.println("BinarySearch is x" + String.format("%.2f", ratio) + " times faster than LinearSearch in sorted array.\n");
+    }
+
+    public void case8192(int value) {
+        int arr[] = generateArray(8192);
+        int linearON, binaryON;
+        System.out.println("\n------------------------CASE 8192------------------------\n");
+        linearON = linearSearch(arr, value);
+        binaryON = binarySearch(arr, value);
+        // if any of them is -1, print error
+        double ratio =  (double) linearON / (double) binaryON;
+        System.out.println("BinarySearch is x" + String.format("%.2f", ratio) + " times faster than LinearSearch in sorted array.\n");
+    }
+
+    private int[] generateArray(int number) {
+        int arr[] = new int[number];
+        for (int i = 0; i < number; i++) {
+            arr[i] = i;
+        }
+        return arr;
     }
 
 }
