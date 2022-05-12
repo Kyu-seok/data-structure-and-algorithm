29ca7da6ca7f8377077d1aae6e1b7c3ba2442d36
Kyu-seok <yeumkyuseok@gmail.com>
2022-05-12 22:52:25 +0900

    update exceptions in binarySearch()

--- SearchingAlgorithms/src/Searching.java
+++ SearchingAlgorithms/src/Searching.java
@@ -1,34 +1,31 @@
 public class Searching {
 
     public int linearSearch(int arr[], int value) {
-        int oN = 0;
-        if (arr == null) {
-            throw new NullPointerException("Null array input");
-        }
-        if (value < 0) {
-            throw new IllegalArgumentException("Entered value is negative");
+        int timeComplexity = 1;
+        if (arr == null || value < 0) {
+            throw new IllegalArgumentException("Invalid input");
         }
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == value) {
-                System.out.println("[LINEAR_SEARCH] : The element is found with O[" + oN + "]");
-                return oN;
+                System.out.println("[LINEAR_SEARCH] : The element is found with the time complexity of O[" + timeComplexity + "]");
+                return timeComplexity;
             }
-            oN++;
+            timeComplexity++;
         }
         System.out.println("[LINEAR_SEARCH] : The element " + value + " not found.");
         return -1;
     }
 
     public int binarySearch(int arr[], int value, boolean isSorted) {
-        int oN = 0;
+        int timeComplexity = 1;
         int start = 0;
         int end = arr.length - 1;
         int middle = (start + end) / 2;
-        if (arr == null) {
-            throw new NullPointerException("Null array input");
-        }
-        if (value < 0 || isSorted == false) {
+        if (value < 0 || isSorted == false || arr == null) {
             throw new IllegalArgumentException("Invalid input");
+        }
+        if (arr.length == 0) {
+            return -1;
         }
         while (arr[middle] != value && start <= end) {
             if (value < arr[middle]) {
@@ -37,11 +34,11 @@ public class Searching {
                 start = middle + 1;
             }
             middle = (start + end) / 2;
-            oN++;
+            timeComplexity++;
         }
         if (arr[middle] == value) {
-            System.out.println("[BINARY_SEARCH] : The element is found with O[" + oN + "]");
-            return oN;
+            System.out.println("[BINARY_SEARCH] : The element is found with the time complexity of O[" + timeComplexity + "]");
+            return timeComplexity;
         } else {
             System.out.println("[BINARY_SEARCH] : The element " + value + " not found.");
             return -1;
@@ -49,7 +46,7 @@ public class Searching {
     }
 
     public void case1024(int value) {
-        int arr[] = generateArray(1024);
+        int arr[] = generateSortedArrayWithoutMultipleOf5(1024);
         int linearON, binaryON;
         System.out.println("\n------------------------CASE 1024------------------------\n");
         linearON = linearSearch(arr, value);
@@ -63,7 +60,7 @@ public class Searching {
     }
 
     public void case8192(int value) {
-        int arr[] = generateArray(8192);
+        int arr[] = generateSortedArrayWithoutMultipleOf5(8192);
         int linearON, binaryON;
         System.out.println("\n------------------------CASE 8192------------------------\n");
         linearON = linearSearch(arr, value);
@@ -79,18 +76,26 @@ public class Searching {
     /**
      * This method returns array of sorted integer except for multiple of 5
      *
-     * @param number The size of array
+     * @param size The size of array
      * @return The generated array of sorted integer
      */
-    public int[] generateArray(int number) {
-        int arr[] = new int[number];
+    public int[] generateSortedArrayWithoutMultipleOf5(int size) {
+        int arr[] = new int[size];
         int num = 0;
-        for (int i = 0; i < number; i++) {
+        for (int i = 0; i < size; i++) {
             if ((num % 5 == 0) && num != 0) {
                 num++;
             }
             arr[i] = num;
             num++;
+        }
+        return arr;
+    }
+
+    public int[] generateSortedArray(int size) {
+        int arr[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = i;
         }
         return arr;
     }
