6a762738a59707d0e31ef76d8038f79f37c9ca4d
Kyu-seok <yeumkyuseok@gmail.com>
2022-05-12 18:13:24 +0900

    Initialize SearchingTest.java

---
+++ SearchingAlgorithms/test/SearchingTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SearchingTest {
+
+    @Test
+    public void linearSearch() {
+    }
+
+    @Test
+    public void binarySearch() {
+    }
+
+}
