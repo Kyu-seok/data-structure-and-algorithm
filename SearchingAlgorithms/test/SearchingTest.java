95af0fd1c92e42818a243d62c94a7b33045dc534
Kyu-seok <yeumkyuseok@gmail.com>
2022-05-12 19:52:29 +0900

    Implement Base Choice Coverage

--- SearchingAlgorithms/test/SearchingTest.java
+++ SearchingAlgorithms/test/SearchingTest.java
@@ -1,15 +1,52 @@
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class SearchingTest {
+class SearchingTest {
 
-    @Test
-    public void linearSearch() {
+    static int arrNull[], arr0[], arr10[];
+    static Searching searching;
+
+    @BeforeAll
+    public static void init() {
+        searching = new Searching();
+        arrNull = null;
+        arr0 = new int[0];
+        arr10 = searching.generateSortedArrayWithoutMultipleOf5(10);
+        System.out.println("Running a test...");
     }
 
     @Test
-    public void binarySearch() {
+    public void linearSearchPrimePathCoverage() {
+        assertEquals(-1, searching.linearSearch(arr10, 19));
+    }
+
+    @Test
+    public void linearSearchBaseChoiceCoverage() {
+        assertEquals(2, searching.linearSearch(arr10, 1));
+        assertThrows(NullPointerException.class, () -> searching.linearSearch(arrNull, 1));
+        assertEquals(-1, searching.linearSearch(arr0, 1));
+        assertThrows(IllegalArgumentException.class, () -> searching.linearSearch(arr10, -1));
+        assertEquals(1, searching.linearSearch(arr10, 0));
+        assertEquals(-1, searching.linearSearch(arr10, 5));
+
+    }
+
+    @Test
+    public void binarySearchPrimePathCoverage() {
+
+    }
+
+    @Test
+    public void binarySearchBaseChoiceCoverage() {
+        assertEquals(2, searching.binarySearch(arr10, 1, true));
+        assertThrows(NullPointerException.class, () -> searching.binarySearch(arrNull, 1, true));
+        assertEquals(-1, searching.binarySearch(arr0, 1, true));
+        assertThrows(IllegalArgumentException.class, () -> searching.binarySearch(arr10, -1, true));
+        assertEquals(3, searching.binarySearch(arr10, 0, true));
+        assertEquals(-1, searching.binarySearch(arr10, 5, true));
+        assertThrows(IllegalArgumentException.class, () -> searching.binarySearch(arr10, 1, false));
     }
 
 }
