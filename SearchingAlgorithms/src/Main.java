58ca503c937cea826dffd7b4c498eb8db3177cdb
Kyu-seok <yeumkyuseok@gmail.com>
2022-06-12 14:25:51 +0900

    Implement graph using adjacency matrix

--- SearchingAlgorithms/src/Main.java
+++ SearchingAlgorithms/src/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         Searching s1 = new Searching();
-        s1.case1024(800);
-        s1.case8192(4000);
+        s1.case1024(801);
+        s1.case8192(4001);
     }
 
 }
