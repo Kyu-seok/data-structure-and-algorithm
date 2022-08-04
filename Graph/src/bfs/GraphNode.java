ffc32602792677314e2183cfe635f4f96b444171
Kyu-seok <yeumkyuseok@gmail.com>
2022-08-04 10:35:41 +0800

    Update node structure

--- Graph/src/bfs/GraphNode.java
+++ Graph/src/bfs/GraphNode.java
@@ -3,6 +3,7 @@ package bfs;
 public class GraphNode {
     public String name;
     public int index;
+    public boolean isVisited = false;
 
     public GraphNode(String name, int index) {
         this.name = name;
