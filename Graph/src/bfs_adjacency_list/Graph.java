959c31f666e921995592b2b84102ac7d607a28e7
Kyu-seok <yeumkyuseok@gmail.com>
2022-08-10 10:56:12 +0800

    Initialize bfs_adjacency_list

---
+++ Graph/src/bfs_adjacency_list/Graph.java
@@ -0,0 +1,36 @@
+package bfs_adjacency_list;
+
+import java.util.ArrayList;
+
+public class Graph {
+
+    ArrayList<GraphNode> nodeList;
+
+    public Graph(ArrayList<GraphNode> nodeList) {
+        this.nodeList = nodeList;
+    }
+
+    public void addUndirectedEdge(int i, int j) {
+        GraphNode first = nodeList.get(i);
+        GraphNode second = nodeList.get(j);
+        first.neighbour.add(second);
+        second.neighbour.add(first);
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < nodeList.size(); i++) {
+            s.append(nodeList.get(i).name + ": ");
+            for (int j = 0; j < nodeList.get(i).neighbour.size(); j++) {
+                if (j == nodeList.get(i).neighbour.size() - 1) {
+                    s.append((nodeList.get(i).neighbour.get(j).name));
+                } else {
+                    s.append((nodeList.get(i).neighbour.get(j).name) + " -> ");
+                }
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+
+}
